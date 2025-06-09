61d55906e1aa25f6ba59f53ce4793d45ae1fd8a4
Castilla33 <mcc0115@alu.medac.es>
2025-06-10 00:56:01 +0200

    Setup
    Realising all the mod setup, ID changed here and there  + gradle build works correctly

--- src/main/java/com/funkyriffsoul/cheeseandsteel/CheeseAndSteel.java
+++ src/main/java/com/funkyriffsoul/cheeseandsteel/CheeseAndSteel.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.funkyriffsoul.cheeseandsteel;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,16 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(CheeseAndSteel.MOD_ID)
+public class CheeseAndSteel
 {
+
+    public static final String MOD_ID = "cheeseandsteel";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public CheeseAndSteel() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -48,7 +51,7 @@ public class ExampleMod
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
+
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
